0f02340c19280276d153cf68b6aa49263ddf0df6
SHREERAM KUMAR KUSHWAHA <vtu16194@veltech.edu.in>
2025-03-02 00:38:49 +0530

    Initial commit - Personal Portfolio Website

---
+++ script.js
@@ -0,0 +1,11 @@
+function toggleTheme() {
+    document.body.classList.toggle('dark-theme');
+}
+
+// Show Project Details When Clicked
+function showProject(id) {
+    let projects = document.querySelectorAll('.project-details');
+    projects.forEach(project => project.classList.remove('active'));
+
+    document.getElementById(id).classList.add('active');
+}
